6ff42eb20ea4965547adcbb5ac91ccb00271a151
iftekher <rocktimcseru50@gmail.com>
2018-09-17 11:51:40 +0600

    interface simple program

---
+++ src/main/Hello.java
@@ -0,0 +1,10 @@
+package main;
+
+public interface Hello {
+	//public static void main(String[] args) {
+		
+	//}
+	
+	String processName (String string);
+
+}
---
+++ src/main/HelloRun.java
@@ -0,0 +1,12 @@
+package main;
+
+public class HelloRun {
+	public static void main (String[] args) {
+		Hello he = (name)-> "Hello Word " + name + "!";
+		System.out.println(he.processName("rocktim"));
+		
+		
+		
+	}
+}
+ 
